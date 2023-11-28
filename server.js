b22462c1d2364786c66e0d13cc3edb04083f13d4
Con5lata <131604666+Con5lata@users.noreply.github.com>
2023-11-28 12:26:36 +0300

    Changes

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log('hi');
+const http = require('http');// provides functionality for creating a server
+const app = require('/app');
+const port = process.env.PORT || 3000;
+
+const SERVER = http.createServer(app);
+
+server.listen(port);
